45e84b667f6dc9b566d7956eeb44b34d661cf339
Maddi Wade <Madelinenwade@gmail.com>
2022-02-15 12:34:41 -0500

    Add project files.

---
+++ SugarSyntax/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SugarSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 12;
+            string response = (answer < 9) ? "is less than nine" : "Greater then or equal to 9";
+            Console.WriteLine(response);
+
+            // it was bothering me there was nothing on my command prompt lol
+          
+
+        }
+    }
+}
